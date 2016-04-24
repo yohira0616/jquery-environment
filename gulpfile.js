a7687077463a0f1dcd872f7f4cdcce3e3e9098e0
yohira0616 <yukito.sys.so@gmail.com>
2016-04-24 16:14:50 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+gulp.task('js', function () {
+
+});
+
+gulp.task('scss', function () {
+
+});
+
+gulp.task('htdocs', function () {
+
+});
+
+gulp.task('build', function () {
+
+});
+
+gulp.task('watch', function () {
+
+});
